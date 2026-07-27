ff264e4edf6cc31e1a3123c450ba680bcc2421d7
danny de <danielde666@users.noreply.github.com>
2026-07-27 13:53:05 +0200

    custom artwork

--- assets/product-custom-property.js
+++ assets/product-custom-property.js
@@ -3,8 +3,10 @@ import { Component } from '@theme/component';
 
 /**
  * @typedef {object} ProductCustomPropertyRefs
- * @property {HTMLInputElement | HTMLTextAreaElement} textInput - The text input.
- * @property {HTMLElement} characterCount - The character count element.
+ * @property {HTMLInputElement | HTMLTextAreaElement} [textInput] - The text input.
+ * @property {HTMLInputElement} [fileInput] - The file input.
+ * @property {HTMLElement} [characterCount] - The character count element.
+ * @property {HTMLElement} [fileName] - The selected file name element.
  */
 
 /**
@@ -16,8 +18,14 @@ class ProductCustomProperty extends Component {
     this.#updateCharacterCount();
   }
 
+  handleFileChange() {
+    this.#updateFileName();
+  }
+
   #updateCharacterCount() {
     const { characterCount, textInput } = this.refs;
+    if (!characterCount || !textInput) return;
+
     const currentLength = textInput.value.length;
     const maxLength = textInput.maxLength;
 
@@ -27,6 +35,22 @@ class ProductCustomProperty extends Component {
     const updatedText = template.replace('[current]', currentLength.toString()).replace('[max]', maxLength.toString());
 
     characterCount.textContent = updatedText;
+  }
+
+  #updateFileName() {
+    const { fileInput, fileName } = this.refs;
+    if (!fileInput || !fileName) return;
+
+    const selectedFile = fileInput.files?.[0];
+    const emptyText = fileName.getAttribute('data-empty-text') || '';
+
+    if (selectedFile) {
+      fileName.textContent = selectedFile.name;
+      fileName.setAttribute('data-has-file', 'true');
+    } else {
+      fileName.textContent = emptyText;
+      fileName.removeAttribute('data-has-file');
+    }
   }
 }
 
